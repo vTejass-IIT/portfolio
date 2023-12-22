08e3f392bd4434c89b21c07995b7d241fd75b997
Tejass Vishwanath <tvishwanath@hawk.iit.edu>
2023-12-21 18:43:48 -0600

    create scrolltoview hook

--- src/components/introduction/introduction.js
+++ src/components/introduction/introduction.js
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from "./introduction.module.scss"
 import useTypewriter from "../../utils/hooks/useTypewritter.js"
 import SpiderMan from '../spiderman/spiderman';
+import useScrollToView from '../../utils/hooks/useScrollToView';
 
 const Introduction = () => {
     const text = "Hello, I'm Tejass";
     const subText = "and this is my portfolio..."
     const displayText = useTypewriter(text, 100);
     const displaySubText = useTypewriter(subText, 100, 2000)
+    const scrollToRef = useScrollToView('overview')
     return (
         <section className={` relative h-[100vh]`}>
             <SpiderMan />
@@ -17,7 +19,7 @@ const Introduction = () => {
                 </h1>
                 <p className={`text-lg md:text-xl`}>{displaySubText}<span className={styles.cursor}>|</span></p>
             </section>
-            <div className={`absolute sm:bottom-20 bottom-10 w-full flex justify-center items-center z-[1]`}><a href="#overview"><div className={`w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2`}><div className="action-slider w-3 h-3 rounded-full bg-secondary mb-1 bg-white"></div></div></a></div>
+            <div className={`absolute sm:bottom-20 bottom-10 w-full flex justify-center items-center z-[1]`}><button ref={scrollToRef}><div className={`w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2`}><div className="action-slider w-3 h-3 rounded-full bg-secondary mb-1 bg-white"></div></div></button></div>
         </section>
     )
 }
